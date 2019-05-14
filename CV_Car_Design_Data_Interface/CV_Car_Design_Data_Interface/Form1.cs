5112c335bc56b27470e25a142492e08ebca5b40c
rplester <wra@PEWS001WASHA>
2019-05-14 15:52:50 -0600

    NLog Ready

--- CV_Car_Design_Data_Interface/CV_Car_Design_Data_Interface/Form1.cs
+++ CV_Car_Design_Data_Interface/CV_Car_Design_Data_Interface/Form1.cs
@@ -12,9 +12,15 @@ namespace CV_Car_Design_Data_Interface
 {
     public partial class Form1 : Form
     {
+        public static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
