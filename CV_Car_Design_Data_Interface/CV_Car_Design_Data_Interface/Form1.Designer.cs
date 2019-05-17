3162065c322cd3ced1a3120f31c0326530a83e57
rplester <wra@PEWS001WASHA>
2019-05-17 11:56:03 -0600

    CSV export and weight query working

--- CV_Car_Design_Data_Interface/CV_Car_Design_Data_Interface/Form1.Designer.cs
+++ CV_Car_Design_Data_Interface/CV_Car_Design_Data_Interface/Form1.Designer.cs
@@ -29,32 +29,28 @@
         private void InitializeComponent()
         {
             this.txtFilePath = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnFileBrowse = new System.Windows.Forms.Button();
             this.btnLoadCSV = new System.Windows.Forms.Button();
-            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
-            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnBrowseExportFolder = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.lblRowsLoaded = new System.Windows.Forms.Label();
+            this.lblTare = new System.Windows.Forms.Label();
+            this.txtCarID = new System.Windows.Forms.TextBox();
+            this.btnQueryCar = new System.Windows.Forms.Button();
+            this.lblLoad = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtFilePath
             // 
-            this.txtFilePath.Location = new System.Drawing.Point(370, 154);
+            this.txtFilePath.Location = new System.Drawing.Point(7, 14);
             this.txtFilePath.Name = "txtFilePath";
             this.txtFilePath.Size = new System.Drawing.Size(134, 20);
             this.txtFilePath.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(444, 279);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
             // btnFileBrowse
             // 
-            this.btnFileBrowse.Location = new System.Drawing.Point(510, 152);
+            this.btnFileBrowse.Location = new System.Drawing.Point(147, 12);
             this.btnFileBrowse.Name = "btnFileBrowse";
             this.btnFileBrowse.Size = new System.Drawing.Size(33, 23);
             this.btnFileBrowse.TabIndex = 2;
@@ -64,7 +60,7 @@
             // 
             // btnLoadCSV
             // 
-            this.btnLoadCSV.Location = new System.Drawing.Point(549, 152);
+            this.btnLoadCSV.Location = new System.Drawing.Point(186, 12);
             this.btnLoadCSV.Name = "btnLoadCSV";
             this.btnLoadCSV.Size = new System.Drawing.Size(115, 23);
             this.btnLoadCSV.TabIndex = 3;
@@ -72,25 +68,97 @@
             this.btnLoadCSV.UseVisualStyleBackColor = true;
             this.btnLoadCSV.Click += new System.EventHandler(this.BtnLoadCSV_Click);
             // 
-            // fileSystemWatcher1
+            // btnExport
             // 
-            this.fileSystemWatcher1.EnableRaisingEvents = true;
-            this.fileSystemWatcher1.SynchronizingObject = this;
-            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.FileSystemWatcher1_Changed);
+            this.btnExport.Location = new System.Drawing.Point(186, 55);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(115, 23);
+            this.btnExport.TabIndex = 6;
+            this.btnExport.Text = "Export CarIDs";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.BtnExport_Click);
+            // 
+            // btnBrowseExportFolder
+            // 
+            this.btnBrowseExportFolder.Location = new System.Drawing.Point(147, 55);
+            this.btnBrowseExportFolder.Name = "btnBrowseExportFolder";
+            this.btnBrowseExportFolder.Size = new System.Drawing.Size(33, 23);
+            this.btnBrowseExportFolder.TabIndex = 5;
+            this.btnBrowseExportFolder.Text = "...";
+            this.btnBrowseExportFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseExportFolder.Click += new System.EventHandler(this.BtnBrowseExportFolder_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(7, 57);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(134, 20);
+            this.txtFolderPath.TabIndex = 4;
+            // 
+            // lblRowsLoaded
+            // 
+            this.lblRowsLoaded.AutoSize = true;
+            this.lblRowsLoaded.Location = new System.Drawing.Point(307, 17);
+            this.lblRowsLoaded.Name = "lblRowsLoaded";
+            this.lblRowsLoaded.Size = new System.Drawing.Size(82, 13);
+            this.lblRowsLoaded.TabIndex = 7;
+            this.lblRowsLoaded.Text = "0 Rows Loaded";
+            // 
+            // lblTare
+            // 
+            this.lblTare.AutoSize = true;
+            this.lblTare.Location = new System.Drawing.Point(255, 106);
+            this.lblTare.Name = "lblTare";
+            this.lblTare.Size = new System.Drawing.Size(39, 13);
+            this.lblTare.TabIndex = 8;
+            this.lblTare.Text = "lblTare";
+            // 
+            // txtCarID
+            // 
+            this.txtCarID.Location = new System.Drawing.Point(7, 103);
+            this.txtCarID.Name = "txtCarID";
+            this.txtCarID.Size = new System.Drawing.Size(134, 20);
+            this.txtCarID.TabIndex = 9;
+            // 
+            // btnQueryCar
+            // 
+            this.btnQueryCar.Location = new System.Drawing.Point(147, 103);
+            this.btnQueryCar.Name = "btnQueryCar";
+            this.btnQueryCar.Size = new System.Drawing.Size(102, 20);
+            this.btnQueryCar.TabIndex = 10;
+            this.btnQueryCar.Text = "Query Car Data";
+            this.btnQueryCar.UseVisualStyleBackColor = true;
+            this.btnQueryCar.Click += new System.EventHandler(this.BtnQueryCar_Click);
+            // 
+            // lblLoad
+            // 
+            this.lblLoad.AutoSize = true;
+            this.lblLoad.Location = new System.Drawing.Point(371, 106);
+            this.lblLoad.Name = "lblLoad";
+            this.lblLoad.Size = new System.Drawing.Size(35, 13);
+            this.lblLoad.TabIndex = 11;
+            this.lblLoad.Text = "label1";
+            this.lblLoad.Click += new System.EventHandler(this.Label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLoad);
+            this.Controls.Add(this.btnQueryCar);
+            this.Controls.Add(this.txtCarID);
+            this.Controls.Add(this.lblTare);
+            this.Controls.Add(this.lblRowsLoaded);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnBrowseExportFolder);
+            this.Controls.Add(this.txtFolderPath);
             this.Controls.Add(this.btnLoadCSV);
             this.Controls.Add(this.btnFileBrowse);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtFilePath);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,10 +167,16 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtFilePath;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnFileBrowse;
         private System.Windows.Forms.Button btnLoadCSV;
-        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnBrowseExportFolder;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label lblRowsLoaded;
+        private System.Windows.Forms.Label lblLoad;
+        private System.Windows.Forms.Button btnQueryCar;
+        private System.Windows.Forms.TextBox txtCarID;
+        private System.Windows.Forms.Label lblTare;
     }
 }
 
--- CV_Car_Design_Data_Interface/CV_Car_Design_Data_Interface/Form1.cs
+++ CV_Car_Design_Data_Interface/CV_Car_Design_Data_Interface/Form1.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 using System.IO;
 using CsvHelper;
 using System.Data.SqlClient;
+using System.Xml;
 
 namespace CV_Car_Design_Data_Interface
 {
     public partial class Form1 : Form
     {
+        //set up logger for error and status logging
         public static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public Form1()
         {
@@ -23,11 +25,13 @@ namespace CV_Car_Design_Data_Interface
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //log application start
             logger.Info("Start");
         }
 
         private void BtnFileBrowse_Click(object sender, EventArgs e)
         {
+            //browse for the file to import to the car database
             OpenFileDialog dlgCSV = new OpenFileDialog();
             dlgCSV.DefaultExt = ".csv";
             if (dlgCSV.ShowDialog() == DialogResult.OK)
@@ -43,15 +47,23 @@ namespace CV_Car_Design_Data_Interface
 
         private void BtnLoadCSV_Click(object sender, EventArgs e)
         {
+            //initialize the row counter
+            int iCount = 0;
 
+            //clear the row count display on the UI
+            lblRowsLoaded.Text = "";
             try
             {
+                //init the stream reader and the csv interpreter
                 StreamReader reader = new StreamReader(this.txtFilePath.Text);
                 var csv = new CsvReader(reader);
            
 
+                //the default cn format has spaces.  Class members have _ instead
                 csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.Replace(" ", "_");
                 csv.Configuration.HasHeaderRecord = true;
+
+                //configuration to handle bad rows
                 csv.Configuration.BadDataFound = context =>
                 {
                     logger.Info(context.RawRow);
@@ -60,27 +72,30 @@ namespace CV_Car_Design_Data_Interface
 
                 try
                 {
-
+                    //get the records from the CSV file
                     IEnumerable < CN_CSV > records = csv.GetRecords<CN_CSV>();
 
+
+                    //sql setup
                     string szSQLConnectionInfo;
                     
                     SqlConnection sql_connection;
 
                     SqlCommand sql_command;
-                    szSQLConnectionInfo = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=CN_Train_Data;Trusted_Connection=true";
+                    szSQLConnectionInfo = GetSQLConnectionInfo();
                     sql_connection = new SqlConnection(szSQLConnectionInfo);
                     sql_connection.Open();
 
+                    //iterate the csv records
                     foreach (CN_CSV item in records)
                     {
                         try
                         {
-
-                        
-                        
+                            //sql command.  Using an SP which removes the oldest copy of this car re
                             sql_command = new SqlCommand("add_update_car", sql_connection);
                             sql_command.CommandType = CommandType.StoredProcedure;
+
+                            //if any numbers are null, set to 0
                             if(item.Door_Width is null)
                             {
                                 item.Door_Width = 0;
@@ -102,37 +117,41 @@ namespace CV_Car_Design_Data_Interface
                                 item.Inside_Height = 0;
                             }
 
+                            //build the SQL command with the class members
                             sql_command.Parameters.Add("@Init", SqlDbType.NVarChar).Value = item.Initials;
-                        sql_command.Parameters.Add("@CarNum", SqlDbType.Int).Value = item.Number;
-                        sql_command.Parameters.Add("@Owner", SqlDbType.NVarChar).Value = item.Owner;
-                        sql_command.Parameters.Add("@AAR_Type", SqlDbType.NVarChar).Value = item.AAR_Type;
-                        sql_command.Parameters.Add("@AAR_Type_Description", SqlDbType.NVarChar).Value = item.AAR_Type_Description;
-                        sql_command.Parameters.Add("@CN_Type", SqlDbType.NVarChar).Value = item.CN_Type;
-                        sql_command.Parameters.Add("@CN_Type_Description", SqlDbType.NVarChar).Value = item.CN_Type_Description;
-                        sql_command.Parameters.Add("@Tare_Weight", SqlDbType.Int).Value = item.Tare_Weight;
-                        sql_command.Parameters.Add("@Load_Limit", SqlDbType.Int).Value = item.Load_Limit;
-                        sql_command.Parameters.Add("@Outside_Length", SqlDbType.Float).Value = item.Outside_Length;
-                        sql_command.Parameters.Add("@Outside_Width", SqlDbType.Float).Value = item.Outside_Width;
-                        sql_command.Parameters.Add("@Outside_Height", SqlDbType.Float).Value = item.Outside_Height;
-                        sql_command.Parameters.Add("@Inside_Length", SqlDbType.Float).Value = item.Inside_Length;
-                        sql_command.Parameters.Add("@Inside_Width", SqlDbType.Float).Value = item.Inside_Width;
-                        sql_command.Parameters.Add("@Inside_Height", SqlDbType.Float).Value = item.Inside_Height;
-                        sql_command.Parameters.Add("@Capacity", SqlDbType.SmallInt).Value = item.Capacity;
-                        sql_command.Parameters.Add("@Capacity_Unit", SqlDbType.NVarChar).Value = item.Capacity_Unit;
-                        sql_command.Parameters.Add("@Floor_Type", SqlDbType.NVarChar).Value = item.Floor_Type;
-                        sql_command.Parameters.Add("@Floor_Type_Description", SqlDbType.NVarChar).Value = item.Floor_Type_Description;
-                        sql_command.Parameters.Add("@Door_Type", SqlDbType.NVarChar).Value = item.Door_Type;
-                        sql_command.Parameters.Add("@Door_Type_Description", SqlDbType.NVarChar).Value = item.Door_Type_Description;
-                        sql_command.Parameters.Add("@Door_Width", SqlDbType.Float).Value = item.Door_Width;
-                        sql_command.Parameters.Add("@Door_Height", SqlDbType.Float).Value = item.Door_Height;
-                        sql_command.Parameters.Add("@Clearance_Restriction", SqlDbType.NVarChar).Value = item.Clearance_Restriction;
-
-                        sql_command.ExecuteNonQuery();
-                         
+                            sql_command.Parameters.Add("@CarNum", SqlDbType.Int).Value = item.Number;
+                            sql_command.Parameters.Add("@Owner", SqlDbType.NVarChar).Value = item.Owner;
+                            sql_command.Parameters.Add("@AAR_Type", SqlDbType.NVarChar).Value = item.AAR_Type;
+                            sql_command.Parameters.Add("@AAR_Type_Description", SqlDbType.NVarChar).Value = item.AAR_Type_Description;
+                            sql_command.Parameters.Add("@CN_Type", SqlDbType.NVarChar).Value = item.CN_Type;
+                            sql_command.Parameters.Add("@CN_Type_Description", SqlDbType.NVarChar).Value = item.CN_Type_Description;
+                            sql_command.Parameters.Add("@Tare_Weight", SqlDbType.Int).Value = item.Tare_Weight;
+                            sql_command.Parameters.Add("@Load_Limit", SqlDbType.Int).Value = item.Load_Limit;
+                            sql_command.Parameters.Add("@Outside_Length", SqlDbType.Float).Value = item.Outside_Length;
+                            sql_command.Parameters.Add("@Outside_Width", SqlDbType.Float).Value = item.Outside_Width;
+                            sql_command.Parameters.Add("@Outside_Height", SqlDbType.Float).Value = item.Outside_Height;
+                            sql_command.Parameters.Add("@Inside_Length", SqlDbType.Float).Value = item.Inside_Length;
+                            sql_command.Parameters.Add("@Inside_Width", SqlDbType.Float).Value = item.Inside_Width;
+                            sql_command.Parameters.Add("@Inside_Height", SqlDbType.Float).Value = item.Inside_Height;
+                            sql_command.Parameters.Add("@Capacity", SqlDbType.SmallInt).Value = item.Capacity;
+                            sql_command.Parameters.Add("@Capacity_Unit", SqlDbType.NVarChar).Value = item.Capacity_Unit;
+                            sql_command.Parameters.Add("@Floor_Type", SqlDbType.NVarChar).Value = item.Floor_Type;
+                            sql_command.Parameters.Add("@Floor_Type_Description", SqlDbType.NVarChar).Value = item.Floor_Type_Description;
+                            sql_command.Parameters.Add("@Door_Type", SqlDbType.NVarChar).Value = item.Door_Type;
+                            sql_command.Parameters.Add("@Door_Type_Description", SqlDbType.NVarChar).Value = item.Door_Type_Description;
+                            sql_command.Parameters.Add("@Door_Width", SqlDbType.Float).Value = item.Door_Width;
+                            sql_command.Parameters.Add("@Door_Height", SqlDbType.Float).Value = item.Door_Height;
+                            sql_command.Parameters.Add("@Clearance_Restriction", SqlDbType.NVarChar).Value = item.Clearance_Restriction;
 
 
-                        
-                        
+                            //execute the query.  Add the number of rows to the rolling count
+                            iCount = iCount + sql_command.ExecuteNonQuery();
+
+
+
+
+
+
                         }
                         catch (Exception exInstance)
                         {
@@ -140,6 +159,11 @@ namespace CV_Car_Design_Data_Interface
                         }
 
                     }
+
+                    //display the number of rows loaded
+                    string szLoaded = iCount.ToString() + " Rows Loaded";
+                    lblRowsLoaded.Text = szLoaded;
+
                     sql_connection.Close();
                 }
                 catch (Exception exParse)
@@ -151,6 +175,140 @@ namespace CV_Car_Design_Data_Interface
             {
                 logger.Error("Error opening file {0}", exFile.Message);
             }
+
+        }
+
+        private void BtnExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //sql server setup
+                SqlConnection sql_connection;
+                DataTable dt = new DataTable();
+                SqlCommand sql_command;
+                string szSQLConnectionInfo;
+
+                //build the query
+                string szTagQuery = "select distinct CONCAT(Initials,CAST(Number as varchar(50))) from Car_Data";
+                szSQLConnectionInfo = GetSQLConnectionInfo();
+                sql_connection = new SqlConnection(szSQLConnectionInfo);
+                sql_command = new SqlCommand(szTagQuery, sql_connection);
+                sql_command.CommandType = CommandType.Text;
+                SqlDataAdapter da = new SqlDataAdapter(sql_command);
+                da.Fill(dt);
+                int iCount = dt.Rows.Count;
+                DateTime dtNow = DateTime.Now;
+                StreamWriter writer = new StreamWriter(this.txtFolderPath.Text + "\\carIDs_" + dtNow.ToString("yyyyMMdd") + ".csv",false);
+                var csv = new CsvWriter(writer);
+
+                // Write columns
+                foreach (DataColumn column in dt.Columns)       
+                {
+                    csv.WriteField(column.ColumnName);
+                }
+                csv.NextRecord();
+
+                // Write row values
+                foreach (DataRow row in dt.Rows)
+                {
+                    for (var i = 0; i < dt.Columns.Count; i++)
+                    {
+                        csv.WriteField(row[i]);
+                    }
+                    csv.NextRecord();
+                }
+                csv.Dispose();
+            }
+            catch (Exception exExport)
+            {
+
+                logger.Error("Error opening file {0}", exExport.Message);
+            }
+
+
+        }
+
+        private void BtnBrowseExportFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFolderPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private string GetSQLConnectionInfo()
+        {
+            //init default connection
+            string szConnectionInfo = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=CN_Train_Data;Trusted_Connection=true";
+            
+
+            //load the xml document
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                //loading the xml file from the EXE directory
+                doc.Load(@"TrainInfo.xml");
+                XmlNodeList n = doc.GetElementsByTagName("SqlConnectionInfo");
+                if (n != null)
+                {
+                    szConnectionInfo = n[0].InnerText;
+                }
+            }
+            catch (Exception Ex)
+            {
+                
+                logger.Error("Error Loading Config settings - {0}", Ex.Message);
+            }
+            return szConnectionInfo;
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnQueryCar_Click(object sender, EventArgs e)
+        {
+
+            this.lblTare.Text = "";
+            this.lblLoad.Text = "";
+            //sql server setup
+            SqlConnection sql_connection;
+            DataTable dt = new DataTable();
+            SqlCommand sql_command;
+            string szSQLConnectionInfo;
+
+            szSQLConnectionInfo = GetSQLConnectionInfo();
+            sql_connection = new SqlConnection(szSQLConnectionInfo);
+            sql_connection.Open();
+            sql_command = new SqlCommand("query_car_weights", sql_connection);
+            sql_command.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter pvCar = new SqlParameter();
+            pvCar.ParameterName = "@CarID";
+            pvCar.DbType = DbType.String;
+            pvCar.Value = this.txtCarID.Text;
+            sql_command.Parameters.Add(pvCar);
+
+
+            SqlParameter pvTare = new SqlParameter();
+            pvTare.ParameterName = "@Tare_Weight";
+            pvTare.DbType = DbType.Int32;
+            pvTare.Direction = ParameterDirection.Output;
+            sql_command.Parameters.Add(pvTare);
+
+            SqlParameter pvLoadLimit = new SqlParameter();
+            pvLoadLimit.ParameterName = "@Load_Limit";
+            pvLoadLimit.DbType = DbType.Int32;
+            pvLoadLimit.Direction = ParameterDirection.Output;
+            sql_command.Parameters.Add(pvLoadLimit);
+
+            int sqlRows = sql_command.ExecuteNonQuery();
+
+            this.lblTare.Text = "Tare - " + sql_command.Parameters["@Tare_Weight"].Value.ToString();
+            this.lblLoad.Text = "Load Limit - " + sql_command.Parameters["@Load_Limit"].Value.ToString();
+            sql_connection.Close();
 
         }
     }
